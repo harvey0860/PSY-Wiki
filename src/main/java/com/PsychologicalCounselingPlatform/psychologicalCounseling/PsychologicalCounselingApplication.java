cdd14060cd4f4e760d5b3922394d00d8cc01d29b
yi <1207442022@qq.com>
2021-12-04 21:27:57 -0800

    1.2 added logback to store logging info

--- src/main/java/com/PsychologicalCounselingPlatform/psychologicalCounseling/PsychologicalCounselingApplication.java
+++ src/main/java/com/PsychologicalCounselingPlatform/psychologicalCounseling/PsychologicalCounselingApplication.java
@@ -1,15 +1,21 @@
 package com.PsychologicalCounselingPlatform.psychologicalCounseling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class PsychologicalCounselingApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(PsychologicalCounselingApplication.class);
+
 	public static void main(String[] args) {
-
-		SpringApplication.run(PsychologicalCounselingApplication.class, args);
-
+		SpringApplication app = new SpringApplication(PsychologicalCounselingApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("App started！！");
+		LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
-
 }
+
